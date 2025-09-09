dc4d3715569e42659b779449615dca0a4de51a6a
Md-Raint-Rubbyat-Sultan <mdranitrubbyatsultan@gmail.com>
2025-09-09 17:11:34 +0600

    scroll related issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { CommonLayout } from "./components/Layouts/CommonLayout/CommonLayout";
-import { Outlet } from "react-router";
+import { Outlet, ScrollRestoration } from "react-router";
 
 type Props = {};
 
 const App: React.FC<Props> = () => {
   return (
     <CommonLayout>
+      <ScrollRestoration />
       <Outlet />
     </CommonLayout>
   );
